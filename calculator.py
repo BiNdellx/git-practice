d8177f27f28bb97d57805eeed0f62c97c6a7342f
BiNdellx <dark_mc@naver.com>
2026-09-18 09:26:05 +0900

    Add div function and edit multiply function

--- calculator.py
+++ calculator.py
@@ -4,9 +4,13 @@ def add(a, b):
 def sub(a, b):
     return a - b
 
-def multiply(a, b):
+def mul(a, b):
     return a * b
+
+def div(a, b):
+    return a / b
 
 print(add(10, 5))
 print(sub(10, 5))
-print(multiply(10, 5))
+print(mul(10, 5))
+print(div(10, 5))
